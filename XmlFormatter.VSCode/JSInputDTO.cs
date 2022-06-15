01c426b8d969921122c0210c9afb61e5c0c5a47a
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2022-06-14 22:25:47 -0400

    some cleanup

--- XmlFormatter.VSCode/JSInputDTO.cs
+++ XmlFormatter.VSCode/JSInputDTO.cs
@@ -2,7 +2,7 @@
 {
     public class JSInputDTO
     {
-        public string XMLString { get; set; }
+        public string? XMLString { get; set; }
         public int? IndentLength { get; set; }
         public bool? UseSelfClosingTags { get; set; }
         public bool? UseSingleQuotes { get; set; }
