be98beb51b242f3bc6e36935e2381974ed13e3a8
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2024-05-11 20:09:14 -0400

    Upgraded to Dotnet 8 (#23)

--- XmlFormatter.CommandLine/Program.cs
+++ XmlFormatter.CommandLine/Program.cs
@@ -9,11 +9,17 @@ public class ConsoleProgram
     public static async Task Main(string[] args)
     {
         string inputString = string.Empty;
+
+        JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
         using (StreamReader reader = new(Console.OpenStandardInput(), Console.InputEncoding))
         {
             inputString = await reader.ReadToEndAsync().ConfigureAwait(false);
         }
-            
+
         if (string.IsNullOrEmpty(inputString))
         {
             throw new Exception("Unable to read text");
@@ -22,28 +28,27 @@ public class ConsoleProgram
         {
             try
             {
-                JsonInputDto? jsonInputDto = JsonSerializer.Deserialize<JsonInputDto>(inputString, options: new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                });
-                if (jsonInputDto == null || string.IsNullOrWhiteSpace(jsonInputDto.XMLString))
+                var jsonInputDto = JsonSerializer.Deserialize<JsonInputDto?>(inputString, options: jsonSerializerOptions);
+                if (jsonInputDto == null || string.IsNullOrWhiteSpace(jsonInputDto.Value.XMLString))
                 {
                     throw new Exception("Unable to parse file");
                 }
-                var formatter = new Formatter(); 
-                switch (jsonInputDto.ActionKind)
+
+                var formatter = new Formatter();
+
+                switch (jsonInputDto.Value.ActionKind)
                 {
                     case FormattingActionKind.Format:
-                        Console.Write(formatter.Format(jsonInputDto.XMLString, jsonInputDto.FormattingOptions));
+                        Console.Write(formatter.Format(jsonInputDto.Value.XMLString, jsonInputDto.Value.FormattingOptions));
                         break;
                     case FormattingActionKind.Minimize:
-                        Console.Write(formatter.Minimize(jsonInputDto.XMLString));
+                        Console.Write(formatter.Minimize(jsonInputDto.Value.XMLString));
                         break;
                     default:
                         throw new Exception("Unsupported action");
                 }
             }
-            catch(XmlException xmlException)
+            catch (XmlException xmlException)
             {
                 throw new Exception($"{xmlException.Message}");
             }
